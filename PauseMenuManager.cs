68799e28d3733ad0a10edab45c2f8535fa203c01
freshvictory <jsren1@verizon.net>
2014-02-15 02:44:19 -0500

    controls the gui and pause menu

---
+++ PauseMenuManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour {
+
+	public GUIText inventoryText, pausedText, restartText, unpauseText, inventoryList, itemNotification;
+
+	public int numFlashlights = 1;
+	public int numPistolAmmo = 30;
+	public int numMachineGunAmmo = 0;
+	public int numLaserGunAmmo = 0;
+	public int numAmmo = 30;
+
+	public MonoBehaviour mouseMove;
+
+	public Vector3 startPosition = new Vector3(0f, 1.08f, 0f);
+
+	void Start () {
+		InventoryManager.numFlashlights = numFlashlights;
+		InventoryManager.numPistolAmmo = numPistolAmmo;
+		InventoryManager.numMachineGunAmmo = numMachineGunAmmo;
+		InventoryManager.numLaserGunAmmo = numLaserGunAmmo;
+		InventoryManager.numAmmo = numAmmo;
+		InventoryManager.itemNotification = itemNotification;
+		Time.timeScale = 1.0f;
+		transform.position = startPosition;
+		InventoryManager.ClearInventory ();
+		inventoryText.enabled = false;
+		pausedText.enabled = false;
+		restartText.enabled = false;
+		unpauseText.enabled = false;
+		inventoryList.enabled = false;
+		itemNotification.enabled = false;
+		mouseMove.enabled = true;
+	}
+
+	void Update () {
+		if (Input.GetKey ("p")) {
+			Time.timeScale = 0.0f;
+			inventoryList.enabled = false;
+			inventoryText.enabled = true;
+			pausedText.enabled = true;
+			restartText.enabled = true;
+			unpauseText.enabled = true;
+			mouseMove.enabled = false;
+		}
+
+		if (Input.GetKey ("return")) {
+			Time.timeScale = 1.0f;
+			inventoryList.enabled = false;
+			inventoryText.enabled = false;
+			pausedText.enabled = false;
+			restartText.enabled = false;
+			unpauseText.enabled = false;
+			mouseMove.enabled = true;
+		}
+
+		if (Input.GetKey ("r") && pausedText.enabled == true) {
+			Application.LoadLevel ("scene1");
+		}
+
+		if (Input.GetKey ("i")) {
+			Time.timeScale = 0.0f;
+			inventoryText.enabled = false;
+			pausedText.enabled = false;
+			restartText.enabled = false;
+			unpauseText.enabled = true;
+			mouseMove.enabled = false;
+			InventoryManager.DisplayInventory (inventoryList);
+			inventoryList.enabled = true;
+		}
+	}
+}
