5aba95324440ecc68f77f2a4e572a95a1c4d9fa9
freshvictory <jsren1@verizon.net>
2014-02-15 02:43:46 -0500

    code on an item to unactivate it and send to the inventory

---
+++ ItemsManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemsManager : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			if (InventoryManager.AddToInventory(gameObject)) {
+				gameObject.SetActive (false);
+			}
+		}
+	}
+}
