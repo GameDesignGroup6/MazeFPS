204082229bec8ef90a22b882fd1cb98731b58564
abc33kr <abc33kr@gmail.com>
2014-02-17 00:44:32 -0500

    VictoryScript commit
    more info on the script as comments

---
+++ VictoryScript.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+
+/**
+ * This script should be attached to an empty object
+ * Also an object that has GridScript attached should be attached to this script
+ * 
+ * This script requires the following behavrios from GridScript
+ * public Vector3 Victory; in the variable declarations
+ * 
+ * a method called
+ * 	void CheckGridSize(){
+		if (VictoryScript.gridSize.x != 0 && VictoryScript.gridSize.z != 0){
+			Size= VictoryScript.gridSize;
+		}
+	}
+ *
+ *This line of code to record where the finish square is at
+ *Victory=PathCells[PathCells.Count-1].position;
+ */
+public class VictoryScript:MonoBehaviour
+{
+	public GridScript grid;//so that a gameObject with GridScript attached can be attached
+	public static Vector3 gridSize;
+	void Start(){
+		//DontDestroyOnLoad(this);
+
+	}
+	
+	
+	/**
+	 *Uses the static Vector3 var from GridScript to check the victory condition
+	 *Has leniency of .5f since it is hard for the character to be at the exact center of the victory square
+	 */
+	void Update(){
+		var playerObj = GameObject.Find ("First Person Controller");//assuming our player is called First Person Controller
+		var playerPos = playerObj.transform.position;
+		//Debug.Log ("Player is at" + playerPos);
+		if (Mathf.Abs(playerPos.x-grid.Victory.x)<.5 && Mathf.Abs(playerPos.z-grid.Victory.z)<.5 && (grid.Victory.x!=0 || grid.Victory.z!=0)){
+			Debug.Log("player reached the end");
+			Victory();
+
+		}
+		StartCoroutine(CheckVic());
+		
+	}
+	/**
+	 *Loads the scene for the next level
+	 */
+	void Victory(){
+		gridSize= new Vector3(grid.Size.x+10,0,grid.Size.z+10);
+		Application.LoadLevel(0);
+	}
+	
+	/**
+	 * waits 1 second before checking the victory condition again
+	 */
+	IEnumerator CheckVic(){
+		yield return new WaitForSeconds(1f);
+	}
+}
